d5accb3e075cc279091abd88ab4fc3856adc13c2
ElenMartyn <elenamartyn00@gmail.com>
2024-10-11 23:41:14 +0700

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
 immutable_var = (1, 2, 3, 4, 5)
 print(immutable_var)
-#immutable_var[0] = 8  #выдает ошибку, так как кортеж неизменяем?  оставила решетку, чтобы код работал
+#immutable_var[0] = 8  #выдает ошибку, так как кортеж неизменяем, оставила решетку, чтобы код работал
 mutable_list = ['a', 'b', 'c', 'f', 'g']
 print(mutable_list)
 mutable_list [4] = 'alphabet'
